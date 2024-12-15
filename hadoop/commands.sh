8a9195cf719942ce687c19606d0c4ddf3e7b439c
Giancarlo Anco Valdivia <giancarlo.anco@ucsp.edu.pe>
2024-12-15 01:19:50 -0500

    Create commands.sh

---
+++ hadoop/commands.sh
@@ -0,0 +1,7 @@
+hdfs dfs -mkdir /datalake
+hdfs dfs -mkdir /datalake/raw
+hdfs dfs -mkdir /datalake/raw/retail
+hdfs dfs -mkdir /datalake/raw/misc
+hdfs dfs -mkdir /datalake/processed
+hdfs dfs -mkdir /datalake/processe/retail
+hdfs dfs -mkdir /datalake/processe/mobile
